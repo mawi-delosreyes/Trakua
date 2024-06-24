a22540bf0fa7fbec12e13ded3a05b0990a4f0310
mawi-delosreyes <mwldelosreyes13@gmail.com>
2024-06-25 04:45:54 +0800

    Initial Backend Commit

---
+++ Backend/src/main/java/com/ChiBao/Envelopes/ChiBaoApplication.java
@@ -0,0 +1,13 @@
+package com.ChiBao.Envelopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiBaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiBaoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ChiBao/Envelopes/ChiBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChiBao.Envelopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiBaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
