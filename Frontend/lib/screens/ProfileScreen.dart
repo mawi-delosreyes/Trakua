4ea630179a3789205391833b6310b9052fd17ab7
mawi-delosreyes <mwldelosreyes13@gmail.com>
2024-08-04 21:59:20 +0800

    Added profile picture

--- Frontend/lib/screens/ProfileScreen.dart
+++ Frontend/lib/screens/ProfileScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/style/ApplicationColors.dart';
 import 'package:frontend/widgets/NavigationBar1.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -14,13 +15,59 @@ class ProfileScreen extends StatelessWidget {
           backgroundColor: ApplicationColors.Background,
           body: new Column(
             children: [
+              new Spacer(
+                flex: 5,
+              ),
               new Flexible(
                 flex: 30,
-                child: Container (
+                child: Column (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    Stack(
+                      alignment: Alignment.center,
+                      children: <Widget> [
+
+                        Container(
+                          height: 100,
+                          width: 100,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: ApplicationColors.Neutrals_300,
+                                width: 1,
+                              ),
+                              shape: BoxShape.circle
+                            ),
+                          ),
+                        Container(
+                          height: 80,
+                          width: 80,
+                          child: SvgPicture.asset(
+                            'assets/icons/user.svg',
+                          fit: BoxFit.scaleDown,
+                          color: ApplicationColors.Primary_900),
+                        ),
+
+                      ]
+                    ),
+                    Container(
+                      child: Text(
+                        "Mawi",
+                        style: TextStyle(
+                          color: ApplicationColors.Primary_900
+                        ),
+                        ),
+                    ),
+
+                    Container()
+
+
+                  ],
+
                 )
               ),
               new Flexible(
-                flex: 50,
+                flex: 40,
                 child: Container (
                   color: ApplicationColors.Secondary_700,
                 )
