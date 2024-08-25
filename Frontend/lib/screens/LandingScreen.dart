e2bc1993376922aca24ecd51ad0da282f6b0d8fa
mawi-delosreyes <mwldelosreyes13@gmail.com>
2024-08-26 00:10:13 +0800

    Initial landing page

---
+++ Frontend/lib/screens/LandingScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  final int user_id;
+
+  LandingScreen({
+    Key? key,
+    required this.user_id
+  });
+
+  @override
+  State<LandingScreen> createState() => _LandingScreen();
+}
+
+class _LandingScreen extends State<LandingScreen> {
+  
+}
