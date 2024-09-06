1f58763b1a476b4e828144e84fdc230e05fba0d7
mawi-delosreyes <mwldelosreyes13@gmail.com>
2024-09-07 01:10:10 +0800

    Added Transaction screen structure

--- Frontend/lib/repository/TransactionRepo.dart
+++ Frontend/lib/repository/TransactionRepo.dart
@@ -46,4 +46,18 @@ class TransactionRepo{
     );
     return sortedTransactionList.map((element) => Transactions.sortedTransactionMap(element)).toList(); 
   }
+
+  Future<List<Transactions>> getTransactionsBasedOnDate(int date) async {
+    Database db = await chibaoDao.dbHelper.getDatabase;
+
+    final sortedDateTransactionList = await db.rawQuery(
+      '''SELECT DISTINCT t.transaction_id, t.envelope_id, e.envelope_name, t.sub_envelope_id, se.sub_envelope_name, 
+      t.transaction_date, t.transaction_amount, t.notes, t.transaction_type, t.account_id, t.from_envelope_id, t.is_transfer, 
+      t.from_sub_envelope_id FROM Transactions t 
+      INNER JOIN Envelopes e ON t.envelope_id=e.envelope_id 
+      INNER JOIN Sub_Envelopes se ON t.sub_envelope_id=se.sub_envelope_id 
+      WHERE t.transaction_date<=${date} ORDER BY t.transaction_date LIMIT 10'''
+    );
+    return sortedDateTransactionList.map((element) => Transactions.fromMap(element)).toList();
+  }
 }
